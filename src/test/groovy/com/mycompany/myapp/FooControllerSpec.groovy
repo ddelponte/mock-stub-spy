9881bc347fd068a6626495dc2d6e97aefae2a0f8
sdelamo <sergio.delamo@softamo.com>
2018-06-22 11:04:21 +0200

    Extract Spy to its own test

--- src/test/groovy/com/mycompany/myapp/FooControllerSpec.groovy
+++ src/test/groovy/com/mycompany/myapp/FooControllerSpec.groovy
@@ -13,20 +13,4 @@ class FooControllerSpec extends Specification implements ControllerUnitTest<FooC
         thrown NullPointerException
     }
 
-    void "Spy FooService"() { // requires implementation of DataTest trait to avoid GORM errors
-        given: "the collaborating service is a Spy"
-        FooService fooService = Spy(FooService)
-
-        and: "the Spy service is set on the controller"
-        controller.fooService = fooService
-
-        when: "the controller action is called"
-        controller.doSomething()
-
-        then: "the Spy can be used to validate cardinality"
-        1 * fooService.doSomething("Sally") // can be in then or given block
-
-        and: "the text from the original service implementation is returned"
-        response.text == "Hi Sally, FooService did something"
-    }
 }
